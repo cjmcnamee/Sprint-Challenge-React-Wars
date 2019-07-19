9b410f9b35a47000c3ef5647c3ec5c9d7854ff00
connor mcnamee <connorjamesmcnamee@gmail.com>
2019-07-19 13:14:56 -0400

    reached mvp

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,12 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
-const Character = ({itemProp}) => {
+export const CoolDiv = styled.div`
+  border-top: 5px dotted black;
+  background-color: rgba(0,0,0,.3);
+`;
 
+export const CoolDiv2 = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-content: center;
+  width: 60%;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const CoolH1 = styled.h1`
+  font-size: 2rem;
+  text-shadow: 1px 1px 5px lightblue;
+`;
+
+export const CoolH2 = styled.h2`
+  font-size: 1rem;
+  text-shadow: 1px 1px 3px lightblue;
+`;
+
+const Character = ({itemProp}) => {
   return (
-    <>
-      <div>{itemProp.name}</div>
-    </>
+    <CoolDiv>
+      <CoolH1>{itemProp.name}</CoolH1>
+      <CoolDiv2>
+        <CoolH2>height: {itemProp.height}</CoolH2>
+        <CoolH2>mass: {itemProp.mass}</CoolH2>
+        <CoolH2>eye color: {itemProp.eye_color}</CoolH2>
+        <CoolH2>skin color: {itemProp.skin_color}</CoolH2>
+        <CoolH2>hair color: {itemProp.hair_color}</CoolH2>
+      </CoolDiv2>
+    </CoolDiv>
   )
 }
 
